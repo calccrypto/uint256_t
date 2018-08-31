f51dbe6ce0743ea3d31cc84e1000ebbc0b489167
Jason Lee <calccrypto@gmail.com>
2018-08-31 14:02:32 -0600

    pedantic

--- uint256_t.h
+++ uint256_t.h
@@ -54,7 +54,7 @@ namespace std {  // This is probably not a good idea
     template <> struct is_arithmetic <uint256_t> : std::true_type {};
     template <> struct is_integral   <uint256_t> : std::true_type {};
     template <> struct is_unsigned   <uint256_t> : std::true_type {};
-};
+}
 
 class uint256_t{
     private:
