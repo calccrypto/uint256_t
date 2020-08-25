005eac1fe68a209585ad0e5593e81ccc2e99b46f
Nemanja Trifunovic <nemanja.trifunovic@fidexx.io>
2020-08-25 21:49:12 +0200

    Made uint256_t and uint128_t types trivially copyable
    This PR makes uint256_t and uint128_t types trivially copyable.
    See: https://en.cppreference.com/w/cpp/named_req/TriviallyCopyable

--- uint128_t.cpp
+++ uint128_t.cpp
@@ -3,38 +3,6 @@
 const uint128_t uint128_0(0);
 const uint128_t uint128_1(1);
 
-uint128_t::uint128_t()
-#ifdef __BIG_ENDIAN__
-    : UPPER(0), LOWER(0)
-#endif
-#ifdef __LITTLE_ENDIAN__
-    : LOWER(0), UPPER(0)
-#endif
-{}
-
-uint128_t::uint128_t(const uint128_t & rhs)
-#ifdef __BIG_ENDIAN__
-    : UPPER(rhs.UPPER), LOWER(rhs.LOWER)
-#endif
-#ifdef __LITTLE_ENDIAN__
-    : LOWER(rhs.LOWER), UPPER(rhs.UPPER)
-#endif
-{}
-
-uint128_t::uint128_t(uint128_t && rhs)
-#ifdef __BIG_ENDIAN__
-    : UPPER(std::move(rhs.UPPER)), LOWER(std::move(rhs.LOWER))
-#endif
-#ifdef __LITTLE_ENDIAN__
-    : LOWER(std::move(rhs.LOWER)), UPPER(std::move(rhs.UPPER))
-#endif
-{
-    if (this != &rhs){
-        rhs.UPPER = 0;
-        rhs.LOWER = 0;
-    }
-}
-
 uint128_t::uint128_t(std::string & s) {
     init(s.c_str());
 }
@@ -78,22 +46,6 @@ uint8_t uint128_t::HexToInt(const char *s) const {
         ret = uint8_t(*s - 'A' + 10);
     }
     return ret;
-}
-
-uint128_t & uint128_t::operator=(const uint128_t & rhs){
-    UPPER = rhs.UPPER;
-    LOWER = rhs.LOWER;
-    return *this;
-}
-
-uint128_t & uint128_t::operator=(uint128_t && rhs){
-    if (this != &rhs){
-        UPPER = std::move(rhs.UPPER);
-        LOWER = std::move(rhs.LOWER);
-        rhs.UPPER = 0;
-        rhs.LOWER = 0;
-    }
-    return *this;
 }
 
 uint128_t::operator bool() const{
